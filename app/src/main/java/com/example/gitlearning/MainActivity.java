c68176664909b8f1b2c8647d9be97588d013bdc1
Kunal Sabhadiya <kunal.sabhadiya@bksd.com>
2024-01-23 11:05:00 +0530

    edited by developer2

--- app/src/main/java/com/example/gitlearning/MainActivity.java
+++ app/src/main/java/com/example/gitlearning/MainActivity.java
@@ -3,16 +3,18 @@ package com.example.gitlearning;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 EditText name,age;
+Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
             name=findViewById(R.id.name);
             age=findViewById(R.id.age);
-
+            submit=findViewById(R.id.submit);
     }
 }
