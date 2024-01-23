9d9a2b436b3cdbbbce515be79bc84d37e2674ff5
Kunal Sabhadiya <kunal.sabhadiya@bksd.com>
2024-01-23 10:59:58 +0530

    edited by developer 1

--- app/src/main/java/com/example/gitlearning/MainActivity.java
+++ app/src/main/java/com/example/gitlearning/MainActivity.java
@@ -4,15 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 EditText name,age;
+TextView tv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
             name=findViewById(R.id.name);
             age=findViewById(R.id.age);
+            tv=findViewById(R.id.tv);
+
+
 
     }
 }
